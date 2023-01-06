9da7fc0043b6d147486f0f3d25d0f56bf9f1d08f
Moises Sousa <moises.sousa583@gmail.com>
2023-01-05 22:12:02 -0300

    Criação crud categorias

--- controllers/CategoryController.py
+++ controllers/CategoryController.py
@@ -38,36 +38,3 @@ class CategoryController:
             return False
         repository = Repository()
         repository.delete("category", category_id)
-
-
-    # def update_product(self, product_id, product_name=None, model=None, category=None, quantity=None):
-    #     product = self.get_products(product_id)
-    #     if not product:
-    #         return False
-
-    #     product = Product(
-    #         product["id"],
-    #         product["product_name"],
-    #         product["model"],
-    #         product["category"],
-    #         product["quantity"]
-    #     )
-
-    #     if product_name:
-    #         product.product_name = product_name
-    #     if model:
-    #         product.model = model
-    #     if category:
-    #         product.quantity = category
-    #     if quantity:
-    #         product.quantity = quantity
-
-    #     repository = Repository()
-    #     repository.update("products", product_id, product.__dict__)
-
-    # def delete_product(self, product_id):
-    #     product = self.get_products(product_id)
-    #     if not product:
-    #         return False
-    #     repository = Repository()
-    #     repository.delete("products", product_id)
