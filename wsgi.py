091515847d06a323e2449f47770090371a2a6dc1
Drakson Bezerra Santos <drakson_bezerra@virgo.atlantico.net.br>
2023-01-23 20:27:50 -0300

    Ajuste no codigo

--- wsgi.py
+++ wsgi.py
@@ -2,9 +2,12 @@ from flask import Flask
 from routers.product_router import bp_product
 from routers.model_router import bp_models
 from routers.category_router import bp_categories
-
+from controllers.ProductMockController import ProductMockController
 
 def create_app():
+    ProductMockController().save_mock()
+
+
     app = Flask(__name__)
 
     with app.app_context():
