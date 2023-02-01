4436a4dd1a078ec422299cafda4664c4f6f5d429
Drakson Bezerra Santos <drakson_bezerra@virgo.atlantico.net.br>
2023-01-31 23:09:06 -0300

    criado rotas para modelos

--- routers/model_router.py
+++ routers/model_router.py
@@ -1,63 +1,55 @@
-# from flask import request, Blueprint
-# from controllers.model.ModelController import ModelController
-# from repository.inMemoryRepository.inMemory import InMemoryRepository
-# from exceptions.api.NotFoundException import NotFoundException
+from flask import jsonify, request, Blueprint
 
-# bp_models = Blueprint("models", __name__)
+from databases.sql_lite import SqlLiteDatabase
+from repositories.modelRepository import ModelRepositorySqlLite
+from controllers.model.ModelController import ModelController
 
-# @bp_models.route('/models', methods=["GET"])
-# def get_model():
-#     model_id =  request.args.get("model_id")
-#     try:
-#         model = ModelController(InMemoryRepository()).get_model(model_id)
-#     except NotFoundException as error:
-#         return {
-#             "message": error.message,
-#         }, error.http_code
+bp_categories = Blueprint("categories", __name__)
 
-#     return {
-#         "models": model
-#     }
 
-# @bp_models.route('/models', methods=["POST"])
-# def save_model():
-#     model_name = request.json["model_name"]
+@bp_categories.route("/model", methods=["POST"])
+def create_model():
+    data_base = SqlLiteDatabase()
+    model_repository = ModelRepositorySqlLite(data_base)
+    model_controller = ModelController(model_repository)
 
-#     try:
-#         ModelController(InMemoryRepository()).save_model(model_name)
-#     except Exception as error:
-#         return{
-#             "message": str(error.message),
-#         }, 500
+    name = request.json.get("name")
+    model = model_controller.create_model(name)
+    return jsonify({"model": model.to_json()})
 
-#     return {
-#         "id": model_name
-#     }, 201
-# @bp_models.route('/models', methods=["PUT"])
-# def update_model():
-#     model_id = request.args.get("model_id")
-#     model_name = request.json.get("model_name")
+@bp_categories.route("/model/int:id", methods=["GET"])
+def get_model(id):
+    data_base = SqlLiteDatabase()
+    model_repository = ModelRepositorySqlLite(data_base)
+    model_controller = ModelController(model_repository)
 
-#     try:
-#         ModelController(InMemoryRepository()).update_model(model_id, model_name)
-#     except NotFoundException as error:
-#         return{
-#             "message": error.message,
-#         },error.http_code
+    model = model_controller.get_model(id)
+    return jsonify({"model": model.to_json()})
 
-#     return {
-#         "id": model_id
-#     },200
+@bp_categories.route("/model/int:id", methods=["PUT"])
+def update_model(id):
+    data_base = SqlLiteDatabase()
+    model_repository = ModelRepositorySqlLite(data_base)
+    model_controller = ModelController(model_repository)
 
-# @bp_models.route('/models', methods=["DELETE"])
-# def delete_model():
-#     model_id = request.args.get("model_id")
+    model_name = request.json.get("name")
+    model = model_controller.update_model(id, model_name)
+    return jsonify({"model": model.to_json()})
 
-#     try:
-#         ModelController(InMemoryRepository()).delete_model(model_id)
-#     except NotFoundException as error:
-#         return {
-#             "message": error.message
-#         }, error.http_code
+@bp_categories.route("/model/int:id", methods=["DELETE"])
+def delete_model(id):
+    data_base = SqlLiteDatabase()
+    model_repository = ModelRepositorySqlLite(data_base)
+    model_controller = ModelController(model_repository)
 
-#     return {}, 204
+    model_controller.delete_model(id)
+    return jsonify({"message": "Model deleted successfully."})
+
+@bp_categories.route("/categories", methods=["GET"])
+def get_all_categories():
+    data_base = SqlLiteDatabase()
+    model_repository = ModelRepositorySqlLite(data_base)
+    model_controller = ModelController(model_repository)
+
+    categories = model_controller.get_all_categories()
+    return jsonify({"categories": [model.to_json() for model in categories] })
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 from flask import Flask
 # from routers.product_router import bp_product
-# from routers.model_router import bp_models
+from routers.model_router import bp_models
 from routers.category_router import bp_categories
 
 def create_app():
@@ -8,7 +8,7 @@ def create_app():
 
     with app.app_context():
         app.register_blueprint(bp_categories)
+        app.register_blueprint(bp_models)
         # app.register_blueprint(bp_product)
-        # app.register_blueprint(bp_models)
 
         return app
