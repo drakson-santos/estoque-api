41facd098ca6c34761ea2d6dd308c851dfe88e21
Drakson Bezerra Santos <drakson_bezerra@virgo.atlantico.net.br>
2023-01-31 23:16:32 -0300

    feat: criado rotas de produtos

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 from flask import Flask
-# from routers.product_router import bp_product
+from routers.product_router import bp_products
 from routers.model_router import bp_models
 from routers.category_router import bp_categories
 
@@ -9,6 +9,6 @@ def create_app():
     with app.app_context():
         app.register_blueprint(bp_categories)
         app.register_blueprint(bp_models)
-        # app.register_blueprint(bp_product)
+        app.register_blueprint(bp_products)
 
         return app
