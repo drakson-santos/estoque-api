d22826c7052b807cac3f56a5052b6eb5ed4e0ff6
Drakson-Santos <darksonbezerra123@gmail.com>
2023-02-04 20:54:16 -0300

    fix: Ajuste ao buscar

--- routers/product_router.py
+++ routers/product_router.py
@@ -3,6 +3,10 @@ from flask import jsonify, request, Blueprint
 from databases.sql_lite import SqlLiteDatabase
 from repositories.productRepository import ProductRepositorySqlLite
 from controllers.product.ProductController import ProductController
+from repositories.categoryRepository import CategoryRepositorySqlLite
+from controllers.category.CategoryController import CategoryController
+from repositories.modelRepository import ModelRepositorySqlLite
+from controllers.model.ModelController import ModelController
 
 bp_products = Blueprint("products", __name__)
 
@@ -30,11 +34,28 @@ def get_all_products():
     product_repository = ProductRepositorySqlLite(data_base)
     product_controller = ProductController(product_repository)
 
+    model_repository = ModelRepositorySqlLite(data_base)
+    model_controller = ModelController(model_repository)
+
+    category_repository = CategoryRepositorySqlLite(data_base)
+    category_controller = CategoryController(category_repository)
+
+    response = []
+
     products = product_controller.get_all_products()
     if products:
-        products = [product.to_json() for product in products]
+        for product in products:
+            product = product.to_json()
+            
+            product_model = model_controller.get_model(product["model"]) 
+            product_category = category_controller.get_category(product["category"])
+            
+            product["model"] = product_model.to_json()
+            product["category"] = product_category.to_json()
+            
+            response.append(product)
 
-    return jsonify({"products": products })
+    return jsonify({"products": response })
 
 @bp_products.route("/product", methods=["GET"])
 def get_product():
