20d6d07e20c4a6582308a1d0e19bb464a01a082a
Drakson-Santos <darksonbezerra123@gmail.com>
2023-02-05 19:20:08 -0300

    feat: Adicionado build da versao de teste

--- main.py
+++ main.py
@@ -4,5 +4,10 @@ from flask_cors import CORS
 app = create_app()
 CORS(app)
 
+@app.route('/')
+def index():
+    print("bbbbbbbbbbbb")
+    return app.send_static_file('index.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
--- wsgi.py
+++ wsgi.py
@@ -4,11 +4,11 @@ from routers.model_router import bp_models
 from routers.category_router import bp_categories
 
 def create_app():
-    app = Flask(__name__)
-
+    app = Flask(__name__, static_folder='../web-development/build', static_url_path='/')
+    
     with app.app_context():
         app.register_blueprint(bp_categories)
         app.register_blueprint(bp_models)
         app.register_blueprint(bp_products)
-
+        
         return app
